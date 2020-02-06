cde8fa492f576813154fd29106c2e6a61a20cf4a
George Izraelian <g.izraelian@gmail.com>
2020-02-05 19:30:21 -0500

    created app, first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch, Redirect,} from 'react-router-dom';
+// import TopBar from './TopBar';
+// import Main from './Main';
+import './css/styles.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Router>
+          <div>
+            <TopBar />
+              <div>
+                <Switch>
+                  <Route path="/main" exact component={Main} />
+                  <Route exact path="/" render={() =><Redirect to='/main'/>} />
+                  <Route path="/custom_orders" exact component={CustomOrders} />
+                  <Route path="/spun_fiber" exact component={SpunFiber} />
+                  <Route path="/applications" exact component={applications} />
+                  <Route path="/metal_fiber" exact component={MetalFiber} />
+                  <Route path="/About" exact component={About} />
+                </Switch>
+              </div>
+          </div>
+        </Router> */}
+      </div>
+    );
+  }
+}
+
+export default App;
