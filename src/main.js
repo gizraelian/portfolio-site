13fb1124ec6bfb63ee693342c7a7f95f2ed1540b
George Izraelian <g.izraelian@gmail.com>
2020-04-10 19:11:37 -0400

    major reorganization of class elements

--- src/main.js
+++ src/main.js
@@ -6,72 +6,64 @@ export default class main extends Component {
     return (
       <div className="main">
 
-        <div className="main__demoBox">
-          <h1 className="main__demoBox--header">George Izraelian</h1>
-          <h1 className="main__demoBox--subHeader">Web Developer</h1>
-          <img src="../assets/photo-of-cn-tower-during-golden-hour-2773226.jpg" className="main__demoBox--background"  alt='myImg'/>
+        <div className="main__splashBox" >
+          <h1 className="main__splashBox--header">George Izraelian</h1>
+          <h1 className="main__splashBox--subHeader">Web Developer</h1>
         </div>
 
 {/* Jumbotron Section */}
-        <div className="main__demoBox">
-          <div className="main__demoBox--jumbotron">
-            <h1 className="main__demoBox--jumbotron__containerOne">
+        <div className="main__jumbotron">
+          <h1 className="main__jumbotron--leftBox">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            </h1>
-            <h3 className="main__demoBox--jumbotron__containerTwo">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit. Habitant morbi tristique senectus et. At auctor urna nunc id cursus metus aliquam eleifend. Et molestie ac feugiat sed lectus vestibulum. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt.  
-            </h3>
-          </div>
+          </h1>
+          <h3 className="main__jumbotron--rightBox">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit. Habitant morbi tristique senectus et. At auctor urna nunc id cursus metus aliquam eleifend. Et molestie ac feugiat sed lectus vestibulum. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. 
+          </h3>
         </div>
 
 {/* Skills section */}
-        <div className="main__demoBox">
-          {/* <img src="../assets/photo-of-seaside-during-daytime-3511543.jpg" className="main__demoBox--background"  alt='myImg'/> */}
-          <div className="main__demoBox--skillsBox">
+        <div className="main__skillsBox">
 
-            <div className="main__demoBox--skillsBox__card">
-              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
-              <h2 className="main__demoBox--skillsBox__card--header">
-                React
-              </h2>
-            </div>
-
-            <div className="main__demoBox--skillsBox__card">
-              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
-              <h2 className="main__demoBox--skillsBox__card--header">
-                JavaScript
-              </h2>
-            </div>
-
-            <div className="main__demoBox--skillsBox__card">
-              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
-              <h2 className="main__demoBox--skillsBox__card--header">
-                HTML5
-              </h2>
-            </div>
-
-            <div className="main__demoBox--skillsBox__card">
-              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
-              <h2 className="main__demoBox--skillsBox__card--header">
-                CSS3 | SASS
-              </h2>
-            </div>
-
-            <div className="main__demoBox--skillsBox__card">
-              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
-              <h2 className="main__demoBox--skillsBox__card--header">
-                NodeJS
-              </h2>
-            </div>
-
+          <div className="main__skillsBox--card">
+            <img src="../assets/react.png" 
+             className="main__skillsBox--card__img"/>
+            <h2 className="main__skillsBox--card__header">React</h2>
           </div>
-       </div>
 
-{/* React, JS, HTML5, CSS, node.js */}
+          <div className="main__skillsBox--card">
+            <img src="../assets/react.png" 
+             className="main__skillsBox--card__img"/>
+            <h2 className="main__skillsBox--card__header">JavaScript</h2>
+          </div>
+
+          <div className="main__skillsBox--card">
+            <img src="../assets/react.png" 
+             className="main__skillsBox--card__img"/>
+            <h2 className="main__skillsBox--card__header">HTML5</h2>
+          </div>
+
+          <div className="main__skillsBox--card">
+            <img src="../assets/react.png" 
+             className="main__skillsBox--card__img"/>
+            <h2 className="main__skillsBox--card__header">CSS3 | SASS</h2>
+          </div>
+
+          <div className="main__skillsBox--card">
+            <img src="../assets/react.png" 
+             className="main__skillsBox--card__img"/>
+            <h2 className="main__skillsBox--card__header">NodeJS</h2>
+          </div>
+
+        </div>
 
 {/* Project portfolio section */}
+        <div className="main__projectsBox">
+
+        </div>
+
+
         <div className="main__demoBox">
-          <img src="../assets/body-of-water-2276928.jpg" className="main__demoBox--background"  alt='myImg'/>
+          {/* <img src="../assets/body-of-water-2276928.jpg" className="main__demoBox--background"  alt='myImg'/> */}
         </div>
       </div>
     )
