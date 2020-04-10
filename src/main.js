475f7cb45cda294f239015383831d95f06c7b364
George Izraelian <g.izraelian@gmail.com>
2020-04-10 18:15:14 -0400

    added cards for skills section

--- src/main.js
+++ src/main.js
@@ -25,19 +25,54 @@ export default class main extends Component {
         </div>
 
 {/* Skills section */}
-<div className="main__demoBox">
-          <img src="../assets/white-bus-beside-concrete-structure-2889806.jpg" className="main__demoBox--background"  alt='myImg'/>
-          
+        <div className="main__demoBox">
+          {/* <img src="../assets/photo-of-seaside-during-daytime-3511543.jpg" className="main__demoBox--background"  alt='myImg'/> */}
+          <div className="main__demoBox--skillsBox">
 
-        </div>
+            <div className="main__demoBox--skillsBox__card">
+              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
+              <h2 className="main__demoBox--skillsBox__card--header">
+                React
+              </h2>
+            </div>
+
+            <div className="main__demoBox--skillsBox__card">
+              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
+              <h2 className="main__demoBox--skillsBox__card--header">
+                JavaScript
+              </h2>
+            </div>
+
+            <div className="main__demoBox--skillsBox__card">
+              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
+              <h2 className="main__demoBox--skillsBox__card--header">
+                HTML5
+              </h2>
+            </div>
+
+            <div className="main__demoBox--skillsBox__card">
+              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
+              <h2 className="main__demoBox--skillsBox__card--header">
+                CSS3 | SASS
+              </h2>
+            </div>
+
+            <div className="main__demoBox--skillsBox__card">
+              <img src="../assets/react.png" className="main__demoBox--skillsBox__card--img" />
+              <h2 className="main__demoBox--skillsBox__card--header">
+                NodeJS
+              </h2>
+            </div>
+
+          </div>
+       </div>
+
+{/* React, JS, HTML5, CSS, node.js */}
 
 {/* Project portfolio section */}
-<div className="main__demoBox">
+        <div className="main__demoBox">
           <img src="../assets/body-of-water-2276928.jpg" className="main__demoBox--background"  alt='myImg'/>
-          
-
         </div>
-
       </div>
     )
   }
