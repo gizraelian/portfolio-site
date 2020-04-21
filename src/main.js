e1f1ce9f5c28c5a4dda6e9577bead6b9780ea634
George Izraelian <g.izraelian@gmail.com>
2020-04-21 13:10:54 -0400

    styling adjustments 5

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Projects from './projects';
-import LandingPage from './landingpage';
 import Jumbotron from './jumbotron';
 import Skills from './skills';
+import { Link } from "react-scroll";
+
 // import ReadyToScroll from './ReadyToScroll';
 // import { Link } from 'react-router-dom';
 
@@ -14,7 +15,37 @@ export default class Main extends Component {
 
         <div className="longBackgroundContainer"/>
 
-        <LandingPage />
+        <div className="main__splashBox" id="landingPage">
+
+        <Link
+            className="main__goUp"
+            to="landingPage"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration= {1500} >
+            <img src="../assets/arrows-grey.png" className="main__goUp--img" alt='myImg' />
+        </Link>
+
+    <div className="main__splashBox--background"></div>
+
+    <div className="main__splashBox--container">
+        <h1 className="main__splashBox--container__header">George Izraelian</h1>
+        <h1 className="main__splashBox--container__subHeader">Web Developer</h1>
+        <Link
+            className="main__splashBox--container__scrollToProjects"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration= {1500} >
+            <img src="../assets/arrows-left_500x500.png" className="main__splashBox--container__scrollDown--img" alt='myImg' />
+        </Link>
+    </div>
+
+</div>
+
+        {/* <LandingPage /> */}
 
         <Jumbotron />
 
